0a6dc15b8aba098a5fc473fdae148e8cf4a35409
agungwpg <agungwpg@gmail.com>
2019-08-27 12:43:55 +0700

    final exercse 3

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
-let data
-const table = document.getElementById('table')
-const response = fetch('https://swapi.co/api/planets/')
-.then(response => {
-    return response.json()
-})
-.then( ({ results }) => {
-    data = results
-    data.forEach(({name,created,diameter,climate,population}) => {
-        let newRow = document.createElement('tr')
-        var tdName = newRow.insertCell(0)
-        var tdCreated = newRow.insertCell(1)
-        var tdDiameter = newRow.insertCell(2)
-        var tdClimate = newRow.insertCell(3)
-        var tdPopulation = newRow.insertCell(4)
-        tdName.innerHTML = name
-        tdCreated.innerHTML = created
-        tdDiameter.innerHTML = diameter
-        tdClimate.innerHTML = climate
-        tdPopulation.innerHTML = population
-        table.appendChild(newRow)
-    });
-})
-const filter = document.getElementById('input-filter')
-filter.addEventListener('change',function(e){
-    console.log(e.target.value)
-})
+const table = document.getElementById('table').getElementsByTagName('tbody')[0]
+const inputFilter = document.getElementById('input-filter')
+const labelStatus = document.getElementById('status')
+
+labelStatus.innerHTML = 'Fetching...'
+
+async function getApiData(){
+    let response = await fetch('https://swapi.co/api/planets/')
+    let data = await response.json()
+    return data
+}
+
+function setTableData(results){
+    table.innerHTML = ''
+    results.forEach( ({name,created,diameter,climate,population}) => {
+        let newRow = table.insertRow(-1)
+        newRow.insertCell(-1).innerHTML = name
+        newRow.insertCell(-1).innerHTML = created
+        newRow.insertCell(-1).innerHTML = diameter
+        newRow.insertCell(-1).innerHTML = climate
+        newRow.insertCell(-1).innerHTML = population
+    })
+}
+
+(async function() {
+    let { results } = await getApiData();
+    if(results){
+        labelStatus.innerHTML = 'Done ...'
+        setTableData(results)
+
+        inputFilter.addEventListener('change',function(event) {
+            let { value } = event.target
+            let filtered = results.filter(({name,created,diameter,climate,population}) => {
+                return (
+                    name.toUpperCase().indexOf(value.toUpperCase()) >= 0 ||
+                    created.toUpperCase().indexOf(value.toUpperCase()) >= 0 ||
+                    diameter.toUpperCase().indexOf(value.toUpperCase()) >= 0 ||
+                    climate.toUpperCase().indexOf(value.toUpperCase()) >= 0 ||
+                    population.toUpperCase().indexOf(value.toUpperCase()) >= 0 
+                )
+            })
+            setTableData(filtered)
+        })
+    }else{
+        labelStatus.innerHTML = 'Failed to fetch ... please refresh the page'
+    }
+})()
+
